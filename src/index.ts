695c4ec8384c8eb1b330351820b754c1b33cfd24
druchan <chandru89new@gmail.com>
2022-09-14 23:06:41 +0530

    Add prettier (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config()
 const fetchDeploy = async () => {
-
   await fetch("https://api.vercel.com/v6/deployments", {
-    "headers": {
-      "Authorization": `Bearer ${process.env.VERCEL_TOKEN}`
+    headers: {
+      Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
     },
-    "method": "get"
-  }).then(res => console.log(res.json().then(console.log)))
-}
-fetchDeploy()
+    method: "get",
+  }).then((res) => console.log(res.json().then(console.log)));
+};
+
+fetchDeploy();
