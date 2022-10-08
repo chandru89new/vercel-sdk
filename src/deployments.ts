2d21b9eac4a27f786336e8fb9396204746f23bd2
Chandru <chandru89new@gmail.com>
2022-10-08 12:30:16 +0530

    Change pagination in list deployments and fix spacing bug

--- src/deployments.ts
+++ src/deployments.ts
@@ -1,27 +1,26 @@
 import { get } from "./utils/fetch";
 import { endpointMap } from "./common";
 import { DeploymentList } from "./types/deployment";
+import { PaginationParameters } from "./utils/pagination";
 
 type ListDeploymentsParams = {
   app?: string;
   from?: number;
-  limit?: number;
   projectId?: string;
   rollbackCandidate: boolean;
   since?: number;
   state?:
     | "BUILDING"
-    | " ERROR"
+    | "ERROR"
     | "INITIALIZING"
-    | " QUEUED"
+    | "QUEUED"
     | "READY"
     | "CANCELED";
   target?: string;
   teamId?: string;
   to?: number;
-  until?: number;
   users?: string;
-};
+} & PaginationParameters;
 
 export const listDeployments = (params?: ListDeploymentsParams) => {
   return get<DeploymentList>(endpointMap.listDeployments, {
