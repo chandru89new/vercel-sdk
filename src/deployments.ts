d73d2b3adc869a09e19c6d5cd8c466089407cc6a
Chandru <chandru89new@gmail.com>
2022-10-09 22:02:21 +0530

    Extract type and fix import source bug

--- src/deployments.ts
+++ src/deployments.ts
@@ -1,26 +1,6 @@
 import { get } from "./utils/fetch";
 import { endpointMap } from "./common";
-import { DeploymentList } from "./types/deployment";
-import { PaginationParameters } from "./utils/pagination";
-
-type ListDeploymentsParams = {
-  app?: string;
-  from?: number;
-  projectId?: string;
-  rollbackCandidate: boolean;
-  since?: number;
-  state?:
-    | "BUILDING"
-    | "ERROR"
-    | "INITIALIZING"
-    | "QUEUED"
-    | "READY"
-    | "CANCELED";
-  target?: string;
-  teamId?: string;
-  to?: number;
-  users?: string;
-} & PaginationParameters;
+import { DeploymentList, ListDeploymentsParams } from "./types/deployment";
 
 export const listDeployments = (params?: ListDeploymentsParams) => {
   return get<DeploymentList>(endpointMap.listDeployments, {
