f95e22a7f53d6c6a323232c22f7bc64f4c9f5157
Chandru <chandru89new@gmail.com>
2022-10-10 09:32:09 +0530

    Simplify pagination type

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,12 +1,4 @@
-type Limit = { limit?: number; next?: never; previous?: never };
-type Next = { next: number; previous?: never; limit?: never };
-type Previous = { previous: number; next?: never; limit?: never };
-type LimitNext = { limit: number; next: number; previous?: never };
-type LimitPrevious = { limit: number; previous: number; next?: never };
-
-export type PaginationParameters =
-  | Limit
-  | Next
-  | Previous
-  | LimitPrevious
-  | LimitNext;
+export type PaginationParameters = { limit?: number } & (
+  | { next?: number; previous?: never }
+  | { next?: never; previous?: number }
+);
